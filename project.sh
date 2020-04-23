3081f61f3c367b1727c5583de615c7f3a58405b2
Nishant Varma <nishant.varma@gmail.com>
2020-04-23 10:31:06 +0530

    Fixups

--- project.sh
+++ project.sh
@@ -3,18 +3,16 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
 run() {
     (
-    cd ${DIR}/examples/browser
+    cd examples/browser
     yarn start --hostname="0.0.0.0" --port="8080" --log-level="fatal"
     )
 }
 
 debug() {
     (
-    cd ${DIR}/examples/browser
+    cd examples/browser
     yarn start --hostname="0.0.0.0" --port="8080" --log-level="debug"
     )
 }
